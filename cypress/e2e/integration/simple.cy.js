0747d61acf3dd95b8dae27198c4dc4ad1b50d840
Mahamud Hasan <fmhasan25@gmail.com>
2022-06-13 11:33:31 -0400

    1

--- cypress/e2e/integration/simple.cy.js
+++ cypress/e2e/integration/simple.cy.js
@@ -2,6 +2,14 @@ describe('Browser Actions', () => {
     it('should load correct url',()=>{
         cy.visit('https://example.com',{timeout:10000})
     })
+    //Assertion: url should include certain text
+    it('should check correct url',function(){
+        cy.url().should('include','example.com')
+    })
+    //Assertion: get something which is visible
+    it('should check for correct element on the page',function(){
+        cy.get('h1').should('be.visible')
+    })
  })
 
- 
+  
